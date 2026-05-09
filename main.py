2b57bcd4b0c53078e230e7af53e4c95c5e7b653d
nova <nova@cutey.com>
2026-05-09 17:36:10 +0100

    finished fuctional prototype;
    Co-authored-by: df-001 <df-001@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from PyQt6.QtCore import QSize
+from PyQt6.QtCore import QSize, QTimer, Qt
 from PyQt6.QtWidgets import (
-    QApplication, QWidget, QMainWindow, QPushButton, QProgressBar, QHBoxLayout, QVBoxLayout
+    QApplication, QWidget, QMainWindow, QPushButton, QSlider, QHBoxLayout, QVBoxLayout
 )
 from PyQt6.QtGui import QIcon
 from player import MusicPlayer
@@ -18,7 +18,6 @@ class MainWindow(QMainWindow):
 
         self.music = MusicPlayer()
         
-
         # stylesheet
         with open("stylesheet.qss", "r") as stylesheet:
             self.setStyleSheet(stylesheet.read())
@@ -32,7 +31,14 @@ class MainWindow(QMainWindow):
         self.nextButton.setFixedSize(QSize(48, 48))
 
         # interface
-        self.progressBar = QProgressBar()
+        self.timeline = QSlider(Qt.Orientation.Horizontal)
+
+        # signals
+        self.playButton.clicked.connect(self.toggle_playback)
+        self.nextButton.clicked.connect(self.next_clicked)
+        self.prevButton.clicked.connect(self.prev_clicked)
+        self.timeline.sliderMoved.connect(self.seek_track)
+        self.timeline.valueChanged.connect(self.autoplay_next)
 
         # set button displays
         self.playButton.setIcon(QIcon("icons/play.svg"))
@@ -54,26 +60,48 @@ class MainWindow(QMainWindow):
 
         mainLayout = QVBoxLayout()
         mainLayout.addLayout(btnLayout)
-        mainLayout.addWidget(self.progressBar)
+        mainLayout.addWidget(self.timeline)
 
         container = QWidget()
         container.setLayout(mainLayout)
         self.setCentralWidget(container)
+
+        self.slider_timer = QTimer()
+        self.slider_timer.timeout.connect(self.update_ui)
+        self.slider_timer.start(100)
+
+        self.music.load_song()
     
     def toggle_playback(self):
-        if self.music.is_playing:
+        if self.music.playing:
             self.music.pause()
-            self.playButton.setIcon(QIcon("icons/pause.svg"))
+            self.playButton.setIcon(QIcon("icons/play.svg"))
         else:
             self.music.play()
-            self.playButton.setIcon(QIcon("icons/play.svg"))
+            self.playButton.setIcon(QIcon("icons/pause.svg"))
     
+    def autoplay_next(self):
+        if int(self.music.position) == int(self.music.duration):
+            self.music.next()
+
     def update_ui(self):
-        curr_pos = self.music.get_position
-        duration = self.music.get_duration
+        if not self.timeline.isSliderDown():
+            curr_pos = self.music.position
+            duration = self.music.duration
+            
+            self.timeline.setRange(0, int(duration))
+            self.timeline.setValue(int(curr_pos))
         
-        self.progressBar.setRange(0, int(duration))
-        self.progressBar.setValue(int(curr_pos))
+        
+    
+    def seek_track(self, value):
+        self.music.set_pos(value)
+    
+    def next_clicked(self):
+        self.music.next()
+    
+    def prev_clicked(self):
+        self.music.prev()
 
         
 
--- player.py
+++ player.py
@@ -1,31 +1,43 @@
 from os import PathLike, listdir
+import magic
 import time
 from just_playback import Playback
 
 
-
+mime = magic.Magic(mime=True)
 class MusicPlayer:
     def __init__(self, song_dir: str | PathLike = "/home/nova/Music/"):
-        self.songDir = song_dir
+        self.song_dir = song_dir
         self.player = Playback()
         self.queue = []
-        self.HEAD = 0
-        self.loadSongs()
-        self.songLoaded = False
+        self.player_head = 0
+        self.load_songs()
+        self.load_song()
+        self.song_loaded = True
     
-    def loadSongs(self):
-        for file in listdir(self.songDir):
-            self.queue.append(self.songDir + file)
+    def load_songs(self):
+        for file in listdir(self.song_dir):
+            if "audio" in mime.from_file(self.song_dir + file):
+                self.queue.append(self.song_dir + file)
     
-    def loadSong(self):
-        self.player.load_file(self.queue[self.HEAD])
-        self.songLoaded = True
+    def load_song(self):
+        self.player.load_file(self.queue[self.player_head])
+        self.song_loaded = True
 
     # basic controls
-    def play(self):
-        if not self.songLoaded:
-            self.loadSong()
-        self.player.play()
+    def play(self, force = False):
+        if force:
+            self.load_song()
+            self.player.play()
+            return
+        if not self.song_loaded:
+            self.load_song()
+            self.player.play()
+        elif self.player.active:
+            self.player.resume()
+        else:
+            self.player_head += 1
+            self.player.play()
 
     def pause(self):
         self.player.pause()
@@ -36,15 +48,28 @@ class MusicPlayer:
     def set_pos(self, seconds):
         self.player.seek(seconds)
     
-    # other weird ass functions
+    def next(self):
+        self.player_head += 1
+        self.play(True)
     
-    def is_playing(self):
+    def prev(self):
+        if self.position >= 5:
+            self.play(True)
+        else:
+            self.player_head -= 1
+            self.play(True)
+    
+    # other weird ass functions
+    @property
+    def playing(self):
         return self.player.playing
     
-    def get_position(self):
+    @property
+    def position(self):
         return self.player.curr_pos
     
-    def get_duration(self):
+    @property
+    def duration(self):
         return self.player.duration
     
 
